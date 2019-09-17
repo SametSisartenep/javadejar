4f1e7c0fd2050922f287872486afc78beb14c2ef
rodri <rgl@antares-labs.eu>
2019-09-17 09:20:46 +0000

    character streams instead.

--- streams/eu/atl/main/Main.java
+++ streams/eu/atl/main/Main.java
@@ -3,19 +3,28 @@ package eu.atl.main;
 import java.io.*;
 
 public class Main {
+	private static final String FILE = "xanadu.txt";
+
 	public static void
 	main(String[] args) throws IOException
 	{
 		FileInputStream in = null;
+		FileReader fin = null;
 		int c;
 
 	try{
-		in = new FileInputStream("xanadu.txt");
+		in = new FileInputStream(FILE);
+		fin = new FileReader(FILE);
 		while((c = in.read()) != -1)
+			System.out.write(c);
+		System.out.write('\n');
+		while((c = fin.read()) != -1)
 			System.out.write(c);
 	}finally{
 		if(in != null)
 			in.close();
+		if(fin != null)
+			fin.close();
 	}
 	}
 }
